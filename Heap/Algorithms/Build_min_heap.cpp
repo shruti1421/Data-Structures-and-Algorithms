4d061d1764f2714e194989ddc74e36dd8f0d611f
Smriti Naik <64346825+shruti1421@users.noreply.github.com>
2021-05-29 17:45:52 +0530

    Create Build_min_heap.cpp

---
+++ Heap/Algorithms/Build_min_heap.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void min_heapify(vector<int> &heap, int i) {
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        int smallest = i;
+
+        if (l >= heap.size() && r >= heap.size())
+                return;
+
+        if (l < heap.size() && heap[l] < heap[i])
+                smallest = l;
+
+        if (r < heap.size() && heap[r] < heap[smallest])
+                smallest = r;
+
+        if (smallest != i)
+        {
+                swap(heap[smallest], heap[i]);
+                min_heapify(heap, smallest);
+        }
+}
+
+void build_min_heap(vector<int> &heap) {
+
+        for (int i = (heap.size() / 2) - 1; i >= 0; i--) {
+                min_heapify(heap, i);
+        }
+}
+
+int main() {
+
+        int n;
+        cin >> n;
+
+        vector<int> heap;
+
+        for (int i = 0; i < n; i++) {
+                int x;
+                cin >> x;
+                heap.push_back(x);
+        }
+
+
+        build_min_heap(heap);
+
+        for (auto& x : heap)
+                cout << x << " ";
+
+        return 0;
+}
